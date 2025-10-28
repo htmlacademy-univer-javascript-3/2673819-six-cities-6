823380f8bd38081e4bd8fb10ffcb1d0948f03ca4
Elizaveta Pavlovskaya <lpavlovskayalp@mail.ru>
2025-10-28 17:27:40 +0500

    add react router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,49 @@
 import MainScreen from '../../pages/main-screen/main-screen.tsx';
-
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const.ts';
+import LoginScreen from '../../pages/login-screen/login-screen.tsx';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen.tsx';
+import OfferScreen from '../../pages/offer-screen/offer-screen.tsx';
+import PrivateRoute from '../private-route.tsx';
+import {HelmetProvider} from 'react-helmet-async';
 type AppProps = {
   offersCount: number;
 }
 
 function App({offersCount}: AppProps) {
   return (
-    <MainScreen offersCount={offersCount}/>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={AppRoute.Root}
+            element={<MainScreen offersCount={offersCount}/>}
+          />
+          <Route
+            path={AppRoute.Login}
+            element={<LoginScreen/>}
+          />
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute
+                authorizationStatus={AuthorizationStatus.NoAuth}
+              >
+                <FavoritesScreen/>
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<OfferScreen/>}
+          />
+          {/*<Route*/}
+          {/*  path="*"*/}
+          {/*  element={NotFoundScreen/>}*/}
+          {/*/>*/}
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
